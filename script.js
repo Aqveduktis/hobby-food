9fbced2589848035e7d44e4b34b1ada6c58ee407
sara <sara.lovens.lovn@gmail.com>
2020-02-21 11:26:50 +0100

    fetching reveiws

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // funtion reacts to button
 
+// curl -X GET --header "Accept: application/json" --header "user-key: "c3c54d8037eb79d54e15629d7d4d607e"" "https://developers.zomato.com/api/v2.1/reviews?res_id=17067963"
 
 //const cityApi = document.getElementById('europe').value
 const today = document.getElementById('currentWeather')
-const CityId = 280;
-const CousineId = 162;
+const CityId = 302;
+const CousineId = 83;
 const apiKey = "c3c54d8037eb79d54e15629d7d4d607e";
+const number = Math.ceil((Math.random() * 80))
 
-const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${CityId}&entity_type=city&cuisines=${CousineId}`
+const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${CityId}&entity_type=city&cuisines=${CousineId}&start=0&count=20`
+
+
+console.log(number)
 
 const fetchRestaurants = () => {
 
@@ -19,28 +24,46 @@ const fetchRestaurants = () => {
             console.log(json)
             console.log(restaurants)
             showing(restaurants)
-            const smallList = filter
+
         })
 }
 
 fetchRestaurants()
 
+const fetchreview = (resKey) => {
+    apiRes = `https://developers.zomato.com/api/v2.1/reviews?res_id=${resKey}`
+    fetch(apiRes, { headers: { "user-Key": apiKey } }).then(res => res.json()).then(json => {
+        console.log(json)
+
+    })
+}
+
+
+
 const showing = (inputT) => {
-    inputT.forEach((item, index, arr) =>{
+    const newList = []
+    inputT.forEach((item, index, arr) => {
         const picture = ``
-        
+        newList.push({
+            id: item.restaurant.id,
+            name: item.restaurant.name,
+            kitchen: item.restaurant.cuisines,
+            photo: item.restaurant.photos[0].photo.url
+
+
+        })
         today.innerHTML += `<article><h1>${item.restaurant.name}</h1>
         <img alt ="food" class = "food" src = "${item.restaurant.photos[0].photo.url}">
-        <p>cuisine: ${item.restaurant.cuisines} || establishment: ${item.restaurant.establishment[0]}</p></article>`
-        
+        <p>cuisine: ${item.restaurant.cuisines} || establishment: ${item.restaurant.establishment[0]} || city: ${item.restaurant.location.city} || price: ${item.restaurant.price_range} </p></article>`
+
     })
-    
+    console.log(newList)
 }
 const modeFunc = () => {
     const myBtn = document.getElementById('button')
     const myThing = document.getElementById('container')
     const myBody = document.getElementById('myBody')
-    if (myBtn.innerHTML === 'Light Mode'){
+    if (myBtn.innerHTML === 'Light Mode') {
         myBtn.innerHTML = 'Dark Mode'
         myBody.style.backgroundColor = 'Aliceblue'
     }
@@ -59,8 +82,8 @@ const modeFunc = () => {
 document.getElementById('button').onclick = modeFunc
 
 document.getElementById('buttonCheap').addEventListener("click", () => {
-    
-    
+
+
 });
 
 
